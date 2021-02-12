fda1a7ee4845520f2853445658ee03ed7bb528f3
Pedro Solorza <pe.solorza@alumnos.duoc.cl>
2021-02-12 17:30:44 -0300

    Card Pokemon lista (Hp, Attack, Defense, Speed)

--- js/app.js
+++ js/app.js
@@ -25,7 +25,10 @@ const fetchData = async (idPokemon) => {
         const pokemon = {
             img: dataPokemon.sprites.front_default,
             nombre: dataPokemon.name,
-            hp: dataPokemon.stats[0].base_stat
+            hp: dataPokemon.stats[0].base_stat,
+            attack: dataPokemon.stats[1].base_stat,
+            defense: dataPokemon.stats[2].base_stat,
+            speed:dataPokemon.stats[5].base_stat
         }
 
         pintarCard(pokemon);
@@ -45,7 +48,11 @@ const pintarCard = (pokemon) => {
     
     clone.querySelector('.card-body-img').setAttribute('src', pokemon.img);
     //clone.querySelector('.card-body-img').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
-    clone.querySelector('.card-body-nombre').innerHTML = `${(pokemon.nombre)} <span>${pokemon.hp}</span>`
+    clone.querySelector('.card-body-nombre').innerHTML = `${(pokemon.nombre)} <span>${pokemon.hp} hp</span>`
+    //TODO Cambiar nombre de social por stats, también hacerlo en html y sass
+    clone.querySelectorAll('.card-footer-social h3')[0].textContent = pokemon.attack;
+    clone.querySelectorAll('.card-footer-social h3')[1].textContent = pokemon.defense;
+    clone.querySelectorAll('.card-footer-social h3')[2].textContent = pokemon.speed;
 
     fragment.appendChild(clone);
     flex.appendChild(fragment);
