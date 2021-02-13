5d324bf6a4f3371ae099261a14ffedbc176a8166
Pedro Solorza <pe.solorza@alumnos.duoc.cl>
2021-02-13 00:16:06 -0300

    Cargado los 150 pokemones

--- js/app.js
+++ js/app.js
@@ -1,59 +1,51 @@
 'use strict'
 
-//Mensaje para comprobar si se carga el JS
-console.log("En la consola");
+const items = document.getElementById('items');
+const template = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
 
 //Espero a que se cargue todo el HTML, luego ejecuta la fetchData
 document.addEventListener('DOMContentLoaded', () => {
-    const random = getRandomInt(1, 151);
-    fetchData(random);
+    fetchPokemon();
 });
 
-const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-
-
-const fetchData = async (idPokemon) => {
-    try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+const fetchPokemon = async () => {
+    const pokemones = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        const res = await fetch(url);
         const dataPokemon = await res.json();
-
-        console.log(dataPokemon);
-
-        const pokemon = {
-            img: dataPokemon.sprites.front_default,
-            nombre: dataPokemon.name,
-            hp: dataPokemon.stats[0].base_stat,
-            attack: dataPokemon.stats[1].base_stat,
-            defense: dataPokemon.stats[2].base_stat,
-            speed:dataPokemon.stats[5].base_stat
-        }
-
-        pintarCard(pokemon);
-
-    } catch (error) {
-        console.log(error);
+        pokemones.push(dataPokemon);
     }
+    
+    const listaPokemon = pokemones.map((data) => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites['front_default'],
+        type: data.types.map((type) => type.type.name).join(' - '),
+        hp: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        defense: data.stats[2].base_stat,
+        speed: data.stats[5].base_stat
+    }));
+
+    pintarCard(listaPokemon);
 }
 
-const pintarCard = (pokemon) => {
-    console.log(pokemon);
-
+const pintarCard = (listaPokemon) => {
     const flex = document.querySelector('.flex');
-    const template = document.querySelector('#template-card').content;
-    const clone = template.cloneNode(true);
-    const fragment = document.createDocumentFragment();
-    
-    clone.querySelector('.card-body-img').setAttribute('src', pokemon.img);
-    //clone.querySelector('.card-body-img').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
-    clone.querySelector('.card-body-nombre').innerHTML = `${(pokemon.nombre)} <span>${pokemon.hp} hp</span>`
-    //TODO Cambiar nombre de social por stats, también hacerlo en html y sass
-    clone.querySelectorAll('.card-footer-social h3')[0].textContent = pokemon.attack;
-    clone.querySelectorAll('.card-footer-social h3')[1].textContent = pokemon.defense;
-    clone.querySelectorAll('.card-footer-social h3')[2].textContent = pokemon.speed;
+    listaPokemon.forEach(poke => {        
+        const clone = template.cloneNode(true);
 
-    fragment.appendChild(clone);
+        clone.querySelector('.card-body-img').setAttribute('src', poke.image);
+        clone.querySelector('.card-body-nombre').innerHTML = `${(poke.name)} <span>${poke.hp} hp</span>`
+        clone.querySelector('.card-body-text').textContent = poke.type;
+
+        //TODO Cambiar nombre de social por stats, también hacerlo en html y sass
+        clone.querySelectorAll('.card-footer-social h3')[0].textContent = poke.attack;
+        clone.querySelectorAll('.card-footer-social h3')[1].textContent = poke.defense;
+        clone.querySelectorAll('.card-footer-social h3')[2].textContent = poke.speed;
+        fragment.appendChild(clone);
+    })    
     flex.appendChild(fragment);
 }
